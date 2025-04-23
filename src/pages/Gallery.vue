0f9d7d7bd07e3ce0b4b73a8bac2ef9178c3c0721
Desarrollo02 Jomar <Des02.Jomar@jomar.local>
2025-04-23 15:46:37 -0600

    imagenes anadidas, setup de importacion

---
+++ src/pages/Gallery.vue
@@ -0,0 +1,15 @@
+<template>
+  <h3>Welcome to the 100 Kanojo Art Museum</h3>
+  <p>An attempt to collect all official artwork for 100 Kanojo in one place.
+    Contact: @meidomai on Discord
+    Please message me if you know of art that's missing, if interested in translating a piece or if you see a
+    translation floating around, or if you have a higher quality version of a piece.</p>
+</template>
+<script setup>
+import { onMounted, ref, toRef, watch } from "vue";
+import { galeria } from 'src/composables/gallery'
+
+
+
+</script>
+<style lang="scss" scoped></style>
